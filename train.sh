44efe12d67449f2fd69087b454f52e4a86687eae
wtq18 <wutianqi1@gmail.com>
2022-07-30 17:10:45 -0500

    script updated

--- train.sh
+++ train.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
-module load cuda/11.1.0
-module load miniconda3
-source activate RoseTTAFold
-
-python train.py --data data/train_lst/model --out_path model --lst 1
+python train.py --data data/train_lst --out_path model --lst 1
